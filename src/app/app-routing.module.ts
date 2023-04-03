e810675db958626582c412e665341f5af3190059
Mridul Mishra <mridulmishra0812@gmail.com>
2023-04-03 15:21:37 +0530

    Signup component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AttendanceComponent } from './attendance/attendance.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'login', component: LoginComponent} ,
+  { path: 'login', component: LoginComponent},
+  {path:'attendance', component: AttendanceComponent},
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
